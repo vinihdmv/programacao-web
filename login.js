08611bb8f106059db044d16842f7231259ec99ce
Vinicius Rodrigues <vinicius.rsilva@sempreceub.com>
2024-08-20 10:00:35 -0300

    alteracoes do fundo login

--- login.js
+++ login.js
@@ -37,8 +37,63 @@ function validaCpf(cpf) {
         alert("Formato inválido!");
         return false;
     }
+    
+    let soma = 0; 
+
+    for (let i = 1; i<=9; i++){
+
+        //console.log(cpf.charAt(i-1));
+        soma = soma + (cpf.chatAt(i-1)*(10-(i-1)));
+    }
+
+    console.log(soma);
+    let resto = soma % 11;
+
+    if (resto < 2){
+        if (cpf.charAt(9)!= 0){
+            alert("cpf invalido");
+            return false;
+        }
+        return true
+    }
+
+    let digitVerficador = 11 - resto;
+    if (digitVerficador < 10 && cpf.charAt(9) != digitVerficador) {
+        alert("CPF inválido");
+        return false;
+    }
+    
+    //multiplicar cada digito do cpf até o 1º digito verificador
+    //por 11, 10, 98 , 8 ... 2
+    //recuperar o resto da divisao da soma de todas as multiplicações
+    //por 11. Se o resto for menor que 2, o 2º digitoverificador deve ser 0. Caso contrario deve ser igual aon resto
+    //o segundo digito verfificador é o cpf.chatAt(10)
+    //o  primeiro digito verificador é o cpf.charAt(9)
+
+    let soma2 = 0;
+
+    for (let i = 1; i <= 10; i++) {
+        soma2 = soma2 + (cpf.charAt(i - 1) * (11 - i));
+    }
+
+    let resto2 = soma2 % 11;
+
+    if (resto2 < 2) {
+        if (cpf.charAt(10) != 0) {
+            alert("CPF inválido");
+            return false;
+        }
+        return true;
+    }
+
+    let digitVerficador2 = 11 - resto2;
+    if (digitVerficador2 < 10 && cpf.charAt(10) != digitVerficador2) {
+        alert("CPF inválido");
+        return false;
+    }
 
     return true;
+
 
 }
 
@@ -56,9 +111,11 @@ function validaridade(idade) {
         return false;
     }
 
+
     return true;
 
 }
+
 function validarnome(nome){
    if (nome.trim() === ""){
        showMessage("Nome não pode ser vazio");
