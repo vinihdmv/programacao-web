ed805676a2f2f7a44e51848f8d522e1601ce5354
Vinicius Rodrigues <vinicius.rsilva@sempreceub.com>
2024-09-03 10:44:39 -0300

    index mudadno

--- ContoleDePonto/js/index.js
+++ ContoleDePonto/js/index.js
@@ -4,9 +4,19 @@ function atualizarDataHora() {
     var opcoes = { timeZone: fusoHorario };
     var hora = agora.toLocaleTimeString('en-US', opcoes);
     var data = agora.toLocaleDateString('en-US', opcoes);
+
+    // Atualiza a data e hora na página inicial
     document.getElementById('dataHora').textContent = data + ' ' + hora;
+
+    // Atualiza a data e hora dentro do dialog
+    var dialogDataHora = document.getElementById('dialogDataHora');
+    if (dialogDataHora) {
+        dialogDataHora.textContent = data + ' ' + hora;
+    }
 }
 
+// Atualiza a data e hora imediatamente
+atualizarDataHora();
 setInterval(atualizarDataHora, 1000);
 document.getElementById('fusoHorario').addEventListener('change', atualizarDataHora);
 
@@ -16,16 +26,16 @@ var dialog = document.getElementById("myDialog");
 // Obtenha o botão que abre o dialog
 var btn = document.getElementById("baterPontoBtn");
 
-// Quando o usuário clicar no botão, abra o dialog
+// Quando o usuário clicar no botão, abra o dialog e atualize a data e hora no dialog
 btn.onclick = function() {
-    dialog.showModal();
+    atualizarDataHora(); // Atualiza a data e hora antes de abrir o diálogo
+    dialog.style.display = 'block'; // Torna o diálogo visível
 }
 
+// Obtenha o botão que fecha o dialog
 var closeBtn = document.getElementById("closeDialogBtn");
 
-
+// Quando o usuário clicar no botão de fechar, feche o dialog
 closeBtn.onclick = function() {
-    dialog.close();
+    dialog.style.display = 'none'; // Torna o diálogo invisível
 }
-
-
